a481f9db436c7cbc5c5f3a9d5c8d156753e26152
Anton Podavalov <a.podavalov@gmail.com>
2013-04-13 17:36:48 +0400

    Force ignore config changes

--- TorrentMonitor/protected/config/main.php
+++ TorrentMonitor/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Torrent Monitor',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -47,19 +47,18 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
 		/*
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),*/
+		// uncomment the following to use a MySQL database
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=torrentmonitor',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'password',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
