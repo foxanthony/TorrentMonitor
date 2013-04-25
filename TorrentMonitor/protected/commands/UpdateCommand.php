bfb875cc7ecd56218fde9608e7acfa298801bf63
Anton Podavalov <a.podavalov@gmail.com>
2013-04-25 22:23:15 +0400

    Prepare updateSubjects() function.

--- TorrentMonitor/protected/commands/UpdateCommand.php
+++ TorrentMonitor/protected/commands/UpdateCommand.php
@@ -2,11 +2,21 @@
 class UpdateCommand extends CConsoleCommand
 {
     /**
-     * @cron 10 * * * *
+     * Perform update action. This function will be run
+     * every 20 minutes by crontab or another scheduler.
+     * @cron 20 * * * *
      */
     public function actionUpdate()
     {
-	// TOOD: actionUpdate is not implemented.
+	$this->updateSubjects();
+    }
+
+    /**
+     * Perform update subjects.
+     */
+    public function updateSubjects()
+    {
+	// TODO: implement update subjects
     }
 }
 ?>
